66de19c1f1119c793de29cf99df4e65bb2906a88
Timo Mämecke <timo@maemecke.com>
2020-05-11 18:16:17 +0200

    Improve wording on qrcode page

--- pages/qrcode.tsx
+++ pages/qrcode.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable jsx-a11y/media-has-caption */
 import * as React from 'react'
+import Head from 'next/head'
 import AppLayout from '@ui/layouts/App'
 import { Text, Box } from '@ui/base'
 
@@ -23,13 +24,15 @@ const QRCodePage: React.FC<{}> = () => {
 
   return (
     <AppLayout sticky={false}>
+      <Head>
+        <title key="title">QR-Code scannen | recover</title>
+      </Head>
       <Box px={4}>
         <Text as="h1" size="l" mt={3} mb={5}>
-          QRCode
+          QR-Code scannen
         </Text>
         <Text size="s" fontWeight="bold" mb={4}>
-          Scanne den QR-Code mit deiner Kamera und du wirst eingecheckt
-          weitergeleitet.
+          Halte einfach deine Kamera auf den QR-Code und wir checken dich ein.
         </Text>
       </Box>
       <video id="video" width="100%" height="100%" />
