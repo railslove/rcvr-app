0b383c499a8af3ee07b3135cc146c25c57d4eb98
Timo Mämecke <timo@maemecke.com>
2020-06-23 15:02:21 +0200

    Test vercel env vars

--- scripts/build.js
+++ scripts/build.js
@@ -5,6 +5,9 @@ const isCareBranch =
   process.env.VERCEL_GITHUB_COMMIT_REF &&
   process.env.VERCEL_GITHUB_COMMIT_REF.startsWith('care/')
 
+console.log('url:', process.env.VERCEL_URL)
+console.log('ref:', process.env.VERCEL_GITHUB_COMMIT_REF)
+
 if (isCareBranch || isCareDomain) {
   console.log('Using BUILD_VARIANT: care')
   process.env.BUILD_VARIANT = 'care'
