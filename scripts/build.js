689e14e3550d60a004730f577fedf82a815bcebe
Timo Mämecke <timo@maemecke.com>
2020-06-23 13:34:52 +0200

    Debug build

--- scripts/build.js
+++ scripts/build.js
@@ -5,8 +5,13 @@ const isCareBranch =
   process.env.VERCEL_GITHUB_COMMIT_REF.startsWith('care/')
 const isCareDomain = process.env.VERCEL_URL === 'care.rcvr.app'
 
+console.log(process.env.VERCEL_GITHUB_COMMIT_REF)
+console.log({ isCareBranch, isCareDomain })
+
 if (isCareBranch || isCareDomain) {
   process.env.BUILD_VARIANT = 'care'
 }
+
+console.log('variant: ', process.env.BUILD_VARIANT)
 
 execSync('npm run next-build', { stdio: 'inherit' })
