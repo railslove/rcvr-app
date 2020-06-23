6a3ed89e7cc606d547c08bac4c608aef2295bde6
Timo Mämecke <timo@maemecke.com>
2020-06-23 13:46:04 +0200

    Remove debug logs

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,15 @@
 const execSync = require('child_process').execSync
 
+const isCareDomain = process.env.VERCEL_URL === 'care.rcvr.app'
 const isCareBranch =
   process.env.VERCEL_GITHUB_COMMIT_REF &&
   process.env.VERCEL_GITHUB_COMMIT_REF.startsWith('care/')
-const isCareDomain = process.env.VERCEL_URL === 'care.rcvr.app'
-
-console.log(process.env.VERCEL_GITHUB_COMMIT_REF)
-console.log({ isCareBranch, isCareDomain })
 
 if (isCareBranch || isCareDomain) {
+  console.log('Using BUILD_VARIANT: care')
   process.env.BUILD_VARIANT = 'care'
+} else {
+  console.log('No automatic BUILD_VARIANT detected. Using default from .env')
 }
-
-console.log('variant: ', process.env.BUILD_VARIANT)
 
 execSync('npm run next-build', { stdio: 'inherit' })
