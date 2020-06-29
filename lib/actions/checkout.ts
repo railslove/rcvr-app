f55b37aa992d79cf48b37d14b3e3fb1cc2fee166
Timo Mämecke <timo@maemecke.com>
2020-06-29 15:59:54 +0200

    Show checkout time from server

--- lib/actions/checkout.ts
+++ lib/actions/checkout.ts
@@ -4,9 +4,12 @@ import * as api from '../api'
 
 export async function checkout(checkin: db.Checkin): Promise<db.Checkin> {
   const leftAt = new Date()
-  await api.patchTicket({ id: checkin.id, leftAt })
+  const ticket = await api.patchTicket({ id: checkin.id, leftAt })
 
-  const checkout = await db.updateCheckin({ id: checkin.id, leftAt })
+  const checkout = await db.updateCheckin({
+    id: checkin.id,
+    leftAt: ticket.leftAt,
+  })
   queryCache.refetchQueries('checkins', { force: true })
   return checkout
 }
