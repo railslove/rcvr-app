d3328c557cba99eca97d87828efc5504c0dd1b9c
Thomas Schrader <thomas@railslove.com>
2020-09-02 19:31:29 +0200

    add whitelabel config

---
+++ ui/whitelabels.ts
@@ -0,0 +1,52 @@
+import LogoRcvr from './svg/logo-rcvr.svg'
+import LogoCare from './svg/logo-care.svg'
+import LogoFresenius from './svg/logo-fresenius.svg'
+
+const envs = {
+  rcvr: {
+    backgroundColor: '',
+    highlightColor: '',
+    Logo: LogoRcvr,
+    logoSmallWidth: '61px',
+    logoSmallHeight: '10px',
+    logoBigWidth: '182px',
+    logoBigHeight: '20px',
+    introText:
+      'Durch die aktuellen Corona-Verordnungen musst du deine Kontaktdaten hinterlegen, wenn Du in einem Betrieb bist der zu Schutzmaßnahmen verpflichtet ist, wie z.B Restaurants. Die App kann auch freiwillig genutzt werden, um die Nachverfolgung zu unterstützen.',
+  },
+  care: {
+    backgroundColor: '#eff3f6',
+    highLightColor: '##F5B743',
+    Logo: LogoCare,
+    logoSmallWidth: '92px',
+    logoSmallHeight: '10px',
+    logoBigWidth: '120px',
+    logoBigHeight: '20px',
+    introText:
+      'Durch die aktuellen Corona-Verordnungen müssen Sie Ihre Kontaktdaten hinterlegen, wenn Sie in einem Betrieb sind der zu Schutzmaßnahmen verpflichtet ist, wie z.B Pflegeeinrichtungen. Die App kann auch freiwillig genutzt werden, um die Nachverfolgung zu unterstützen.',
+  },
+  fresenius: {
+    backgroundColor: '#A6D7D7',
+    highlightColor: '#009EE0',
+    Logo: LogoFresenius,
+    logoSmallWidth: '150px',
+    logoSmallHeight: '66px',
+    logoBigWidth: '300px',
+    logoBigHeight: '130px',
+    introText:
+      'Bitte geben Sie Ihre Kontaktdaten ein, wenn Sie sich in diesem Raum aufhalten. Dies ist Teil der verpflichtenden Hygiene- und Schutzmaßnahmen am Campus und dient der Nachverfolgung in einem Infektionsfall.',
+  },
+}
+
+const {
+  introText,
+  backgroundColor,
+  highlightColor,
+  Logo,
+  logoBigWidth,
+  logoBigHeight,
+  logoSmallWidth,
+  logoSmallHeight,
+} = envs[process.env.NEXT_PUBLIC_BUILD_VARIANT]
+
+export { introText, Logo, backgroundColor, highlightColor, logoBigWidth, logoBigHeight, logoSmallWidth, logoSmallHeight, LogoCare, LogoRcvr, LogoFresenius }
