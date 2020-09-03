8975a700dcfec2289c9ebafde2f6a3698672ac91
Thomas Schrader <thomas@railslove.com>
2020-09-03 13:42:37 +0200

    update config to try another way to identify build environment

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,5 @@
 export const isCareEnv = process.env.NEXT_PUBLIC_BUILD_VARIANT === 'care'
 export const isRcvrEnv = process.env.NEXT_PUBLIC_BUILD_VARIANT === 'rcvr'
 export const isFreseniusEnv =
+  process.env.VERCEL_URL.includes('fresenius') ||
   process.env.NEXT_PUBLIC_BUILD_VARIANT === 'fresenius'
