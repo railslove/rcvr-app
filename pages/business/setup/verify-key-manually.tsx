6333137f98d826eb4a7f0ab7338b07023ca38282
Bodo Tasche <bodo@wannawork.de>
2021-04-15 14:31:27 +0200

    Key verification shows wrong length for key
    closes [#119](https://github.com/railslove/recover-backlog/issues/119)

--- pages/business/setup/verify-key-manually.tsx
+++ pages/business/setup/verify-key-manually.tsx
@@ -30,8 +30,10 @@ const VerifyKeyPage: React.FC<WithOwnerProps> = ({ owner }) => {
           .then(() => window.scrollTo(0, 0))
       }
     },
-    [router, owner.privateKey]
+    [router, owner, owner.privateKey]
   )
+
+  console.log(owner)
 
   return (
     <MobileApp>
@@ -56,8 +58,9 @@ const VerifyKeyPage: React.FC<WithOwnerProps> = ({ owner }) => {
       <Box height={4} />
       <Text>
         Zur Erinnerung: {isFormal ? 'Ihr' : 'Dein'} Schlüssel ist{' '}
-        <strong>{base64ToHex(owner.publicKey).length} Zeichen</strong> lang. Er
-        beinhaltet nur Zahlen von <strong>0 bis 9</strong> und Buchstaben von
+        <strong>{base64ToHex(owner.setupPublicKey).length} Zeichen</strong>{' '}
+        lang. Er beinhaltet nur Zahlen von <strong>0 bis 9</strong> und
+        Buchstaben von
         <strong> A bis F</strong>.
       </Text>
       <Box height={6} />
