82bf13196c5c2f334d48dbfcc6c1d9fb0cd2bf8e
Bodo Tasche <bodo@wannawork.de>
2021-05-12 16:45:04 +0200

    Encrypt CWA token in database
    references railslove/recover-backlog#138

--- lib/db/checkin.ts
+++ lib/db/checkin.ts
@@ -4,13 +4,17 @@ import { db, Guest } from './'
 export interface Checkin {
   id?: string
   business?: string
+  address?: string
+  locationType?: string
   enteredAt?: Date
   areaId?: string
   leftAt?: Date
   proxyCheckin?: boolean
   guest?: Guest
-  companyCwaUrl?: string
   k?: string
+  cwaSeed?: string
+  autoCheckoutTime?: number
+  cwaLinkEnabled?: boolean
 }
 
 export async function getCheckin(checkinId: Checkin['id']): Promise<Checkin> {
