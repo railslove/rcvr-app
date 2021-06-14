9c3b8bc03c97924ee973368e22002c3ad7b81da8
Thorsten <thorsten@railslove.com>
2021-06-14 15:54:27 +0200

    made lint happy

--- pages/business/setup/keys.tsx
+++ pages/business/setup/keys.tsx
@@ -1,9 +1,5 @@
 import * as React from 'react'
-
-
 import Head from 'next/head'
-
-
 import { isFormal } from '~lib/config'
 import { WithOwnerProps, withValidPrivateKey } from '~lib/pageWrappers'
 
@@ -14,22 +10,20 @@ import { KeyViewer } from '~ui/blocks/KeyViewer'
 
 const SetupKeysPage: React.FC<WithOwnerProps> = ({ owner }) => {
   const { privateKey } = owner
-  const css_inl= '@media print { .no-print{ display: none !important;} }'
+  const css_inl = '@media print { .no-print{ display: none !important;} }'
   return (
     <MobileApp>
-       
       <Head>
         <title key="title">
           {isFormal ? 'Ihr' : 'Dein'} Schlüssel | recover
         </title>
-     
       </Head>
       <Text as="h2" variant="h2">
         {isFormal ? 'Ihr' : 'Dein'} geheimer Schlüssel
       </Text>
       <Box height={6} />
       <>
-        <Text >
+        <Text>
           <p>
             <strong>
               {isFormal
@@ -60,24 +54,19 @@ const SetupKeysPage: React.FC<WithOwnerProps> = ({ owner }) => {
         <Box height={6} />
 
         <ButtonLink
-          className='no-print'
+          className="no-print"
           href="/business/setup/verify-key-manually"
           right={<ArrowsRight color="green" />}
         >
           Schlüssel prüfen
         </ButtonLink>
-        <Button 
-        className='no-print'
-            onClick={window.print}>
-                    Schlüssel drucken
-          </Button>
-         
+        <Button className="no-print" onClick={window.print}>
+          Schlüssel drucken
+        </Button>
       </>
       <style>{css_inl}</style>
     </MobileApp>
   )
 }
-
-
 
 export default withValidPrivateKey()(SetupKeysPage)
