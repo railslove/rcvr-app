6da4509b7e4f37752e9216d95708c5d78938b43b
Jan Kus <jan@railslove.com>
2021-07-29 09:20:40 +0200

    added polish as language

---
+++ locales/pl/health.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import { Box, ButtonLink, Card, Row, Text } from '~ui/core'
+
+export function indexIntro() {
+    return (
+    <>
+    recover to cyfrowa lista kontaktów dla szpitali, i ich gości. Łatwo, bezpiecznie, szybko.
+    </>
+    )
+}
