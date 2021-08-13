cd68c257ca15ce067af01eda48ff03cd35022b58
Javier Carrillo Milla <stringparser@gmail.com>
2021-08-13 09:58:03 +0200

    cleanup lint stagged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  '*.@(ts|tsx)': ['yarn lint', 'yarn format'],
-  '*.{js,css,md}': 'prettier --write',
   '*.{js,jsx,ts,tsx}': 'eslint --cache --fix',
   '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
 }
