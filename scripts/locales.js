52154caa886e1e58bab01587812474d293dd4933
Javier Carrillo Milla <stringparser@gmail.com>
2021-08-18 17:59:39 +0200

    add missing folder when building

--- scripts/locales.js
+++ scripts/locales.js
@@ -1,3 +1,7 @@
+const { execSync } = require('child_process')
+
+execSync('mkdir -p locales/generated', { stdio: 'inherit' })
+
 const { generateLocalesConfigAndTypes } = require('../locales/generate')
 
 generateLocalesConfigAndTypes()
