7b958dbe3705e60caa03515e1fc292891ab51ad1
Javier Carrillo Milla <stringparser@gmail.com>
2021-08-23 14:53:00 +0200

    cleanup old files

--- pages/qr.de.ts
+++ pages/qr.de.ts
@@ -1,9 +1,11 @@
+import { isFormal } from '~lib/config'
+
 export default {
   pageTitle: 'QR-Code scannen',
   scanCode: 'Scan QR code',
   invalidQRCode:
     'Warnung, dieser QR code ist nicht Teil der RecoverApp. Sie können diese Seite öffnen, aber alle Daten, die Sie dort eingeben, werden an {{hostname}} geschickt.',
-  scanCodeArea:
-    'Scanne den QR-Code, den Du auf dem Tisch teilnehmender Betriebe findest.',
-  scanCodeArea_formal: 'Scannen Sie den QR-Code im Eingangsbereich.',
+  scanCodeArea: isFormal
+    ? 'Scannen Sie den QR-Code im Eingangsbereich.'
+    : 'Scanne den QR-Code, den Du auf dem Tisch teilnehmender Betriebe findest.',
 }
--- pages/qr.en.ts
+++ pages/qr.en.ts
@@ -1,3 +1,4 @@
+import { isFormal } from '~lib/config'
 import qrDe from '~pages/qr.de'
 
 const en: typeof qrDe = {
@@ -5,9 +6,9 @@ const en: typeof qrDe = {
   scanCode: 'Scan QR code',
   invalidQRCode:
     'Warning, this QR code is not part of the RecoverApp. You can open this page, but any data you enter there will be sent to {{hostname}}',
-  scanCodeArea:
-    'Scan the QR code you find on the table of participating establishments',
-  scanCodeArea_formal: 'Scan the QR code found in the entrance area.',
+  scanCodeArea: isFormal
+    ? 'Scan the QR code found in the entrance area.'
+    : 'Scan the QR code you find on the table of participating establishments',
 }
 
 export default en
