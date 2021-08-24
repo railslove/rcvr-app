74cd272228d3d27fa39da6a7c1115821b2470afd
Javier Carrillo Milla <stringparser@gmail.com>
2021-08-24 11:35:26 +0200

    last review for whitelabel

--- ui/whitelabels.ts
+++ ui/whitelabels.ts
@@ -3,21 +3,37 @@ const envs = {
     backgroundColor: 'bluegrey.50',
     primaryHighlightColor: '#28EE5F',
     secondaryHighlightColor: '#EA28EE',
+
+    /* non-theme constants */
+    PDF_TYPE: 'Zusatz-Informationen',
+    PRIVACY_URL: 'https://railslove.com/privacy/',
   },
   care: {
     backgroundColor: '#f2f2f2',
     primaryHighlightColor: '#F5B743',
     secondaryHighlightColor: '',
+
+    /* non-theme constants */
+    PDF_TYPE: 'Hygienevorschriften',
+    PRIVACY_URL: 'https://www.recovercare.de/datenschutzerklarung',
   },
   health: {
     backgroundColor: '#f2f2f2',
     primaryHighlightColor: '#28EE5F',
     secondaryHighlightColor: '#10D4FF',
+
+    /* non-theme constants */
+    PDF_TYPE: 'Hygienevorschriften',
+    PRIVACY_URL: 'https://www.recover-health.de/datenschutzerklarung',
   },
   fresenius: {
     backgroundColor: '#A6D7D7',
     primaryHighlightColor: '#009EE0',
     secondaryHighlightColor: '',
+    /* non-theme constants */
+    PDF_TYPE: 'Zusatz-Informationen',
+    PRIVACY_URL:
+      'https://www.hs-fresenius.de/datenschutzerklaerung-recover-app/',
   },
 }
 
@@ -26,33 +42,11 @@ export type WhiteLabelBuildVariant = keyof typeof envs
 export const BUILD_VARIANT: WhiteLabelBuildVariant =
   (process.env.NEXT_PUBLIC_BUILD_VARIANT as WhiteLabelBuildVariant) || 'rcvr'
 
-const { backgroundColor, primaryHighlightColor, secondaryHighlightColor } =
-  envs[process.env.NEXT_PUBLIC_BUILD_VARIANT || 'rcvr']
-
-export { backgroundColor, primaryHighlightColor, secondaryHighlightColor }
-
-export function buildSelect<T>(variants: Record<WhiteLabelBuildVariant, T>) {
-  return variants[BUILD_VARIANT]
-}
-
-export const PDF_TYPE = buildSelect({
-  rcvr: 'Zusatz-Informationen',
-  care: 'Hygienevorschriften',
-  health: 'Hygienevorschriften',
-  fresenius: '',
-})
-
-export const PRIVACY_URL = (() => {
-  switch (BUILD_VARIANT) {
-    case 'care':
-    case 'health': {
-      return 'https://www.recovercare.de/datenschutzerklarung'
-    }
-    case 'fresenius': {
-      return 'https://www.hs-fresenius.de/datenschutzerklaerung-recover-app/'
-    }
-    default: {
-      return 'https://railslove.com/privacy/'
-    }
-  }
-})()
+export const {
+  backgroundColor,
+  primaryHighlightColor,
+  secondaryHighlightColor,
+  /* non-theme constants */
+  PDF_TYPE,
+  PRIVACY_URL,
+} = envs[BUILD_VARIANT]
