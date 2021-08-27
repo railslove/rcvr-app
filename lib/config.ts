ccde5f1a6ce2bf9f1789dee17e72e5e0d0f1d0e1
Thorsten <thorsten@railslove.com>
2021-08-27 14:12:11 +0200

    made linter happy

--- lib/config.ts
+++ lib/config.ts
@@ -4,4 +4,4 @@ export const isFreseniusEnv =
   process.env.NEXT_PUBLIC_BUILD_VARIANT === 'fresenius'
 export const isHealthEnv = process.env.NEXT_PUBLIC_BUILD_VARIANT === 'health'
 export const isFormal = isCareEnv || isHealthEnv
-export const vercel_url = process.env.VERCEL_URL || ''
+export const vercel_url = process.env.NEXT_PUBLIC_VERCEL_URL || ''
