9ae0bf4915272973a4e9028bfea5b32b2b9404e4
Javier Carrillo Milla <stringparser@gmail.com>
2021-09-09 12:21:19 +0200

    i18n user language (#313)
    * adds `usePersistLocaleCookie`
    
    * use de as default language
    
    * make clear when a locale key is missing

---
+++ locales/defaults.js
@@ -0,0 +1,8 @@
+/**
+ * this will be used in next.config.js and other places
+ * see: https://nextjs.org/docs/advanced-features/i18n-routing#locale-strategies
+ */
+exports = module.exports = {
+  locales: ['de', 'en'],
+  defaultLocale: 'de',
+}
